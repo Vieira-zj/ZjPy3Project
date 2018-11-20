5ffb38f209835750f6a9d1778f379e58b017a685
zhengjin <zivieira@163.com>
2018-11-20 10:17:01 +0800

    Add chart demo.

--- pydemos/py_demo01.py
+++ pydemos/py_demo01.py
@@ -39,6 +39,34 @@ def usage():
     print('\n'.join(lines))
 
 
+def chart_demo():
+    '''
+    pre-conditions: 
+    $ pip install numpy
+    $ pip install matplotlib
+    '''
+    import numpy as np
+    print('numpy version: ' + np.__version__)
+
+    import matplotlib
+    print('matplotlib version: ' + matplotlib.__version__)
+
+    import matplotlib.pyplot as plt
+
+    plt.title('Chart Test')
+    plt.xlabel('X_label_text\n green: system_cpu, blue: user_cpu')
+    plt.ylabel('Y_label_text')
+    
+    x_arr = [x for x in range(0, 10)]
+    y_arr = [y for y in range(0, 20) if y % 2 == 0]
+    z_arr = [y for y in range(0, 20) if y % 2 != 0]
+    plt.plot(x_arr, y_arr, color='red')
+    plt.plot(x_arr, z_arr, color='blue')
+    plt.grid(True, color='green', linestyle='--', linewidth='1')
+    plt.show()
+
+
 if __name__ == '__main__':
     
-    cmd_args_parse()
+#     cmd_args_parse()
+    chart_demo()
