054286bd197bf4a6ff7e094de9d15e55705c4b9e
zhengjin <zivieira@163.com>
2019-03-06 19:38:51 +0800

    Update load configs fn.

--- interfacetest/common/load_configs.py
+++ interfacetest/common/load_configs.py
@@ -12,42 +12,50 @@ import configparser
 sys.path.append('../../')
 from utils import Constants
 from utils import LogManager 
-from utils import SysUtils
 
 
 class LoadConfigs(object):
 
-    __GROUP_HTTP_SERVER_TEST = 'http-svc-test'
+    SECTION_TEST = 'test'
+    SECTION_PROPHET = 'prophet'
 
-    def __init__(self, cfg_file_path=''):
+    # save all configs
+    test_configs = {}
+
+    def __init__(self, logger, cfg_file_path=''):
         if len(cfg_file_path) == 0:
             cfg_file_path = os.path.join(os.path.dirname(os.getcwd()), 'configs.ini')
-
-        log_file = 'test_log_%s' % SysUtils.get_current_date()
-        self.__log_file_path = os.path.join(os.path.dirname(os.getcwd()), 'outputs', 'logs', log_file)
-        self.__log_manager = LogManager(self.__log_file_path)
-        self.__logger = self.__log_manager.get_logger()
-
-        self.__cfg_reader = configparser.ConfigParser()
-        self.__load_configs(cfg_file_path)
-
-    def __load_configs(self, cfg_file_path):
         if not os.path.exists(cfg_file_path):
             raise FileNotFoundError('configs file %s is not exist!' % cfg_file_path)
 
-        self.__logger.info('load configs in file:', cfg_file_path)
+        self.__logger = logger
+        self.__load_configs(cfg_file_path)
+
+    def __load_configs(self, cfg_file_path):
+        self.__logger.info('load configs from: ' + cfg_file_path)
+        self.__cfg_reader = configparser.ConfigParser()
         self.__cfg_reader.read(cfg_file_path)
 
+        for section in self.__cfg_reader.sections():
+            options = self.__cfg_reader.options(section)
+            tmp_dict = {}
+            for option in options:
+                tmp_dict[option] = self.__cfg_reader.get(section, option)
+            self.test_configs[section] = tmp_dict
+
     def get_svc_test_ip(self):
-        return self.__cfg_reader.get(self.__GROUP_HTTP_SERVER_TEST, 'ip')
+        return self.test_configs.get(self.SECTION_TEST).get('ip')
 
     def get_svc_test_port(self):
-        return self.__cfg_reader.get(self.__GROUP_HTTP_SERVER_TEST, 'port')
+        return self.test_configs.get(self.SECTION_TEST).get('port')
 
 
 if __name__ == '__main__':
 
-    cfg = LoadConfigs()
+    log_manager = LogManager(Constants.LOG_FILE_PATH)
+    cfg = LoadConfigs(log_manager.get_logger())
     print('test http server url: %s:%s' % (cfg.get_svc_test_ip(), cfg.get_svc_test_port()))
+    print('prophet user id:', cfg.test_configs.get(LoadConfigs.SECTION_PROPHET).get('id'))
 
+    log_manager.clear_log_handles()
     print('read ini configs DONE.')
