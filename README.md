97a306080308b436a4f9e85e09252d3651bad338
zhengjin <zivieira@163.com>
2019-03-12 14:45:44 +0800

    Add comments

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
-# zj_py3_project
+# zj\_py3\_project
 
-The project includes python3 and pytest demos, and monkey test and api test framework.
+The project includes python3 and pytest demos, and monkey test tool and api test framework (request + pytest + allure).
 
 Note: before run, set env var PYPATH to the current project abs path.
--- apitest/runtests.sh
+++ apitest/runtests.sh
@@ -6,8 +6,12 @@ cd ${HOME}/Workspaces/zj_py3_project/apitest
 # run test_base.py first to init env and load data
 pytest -v -s testcases/test_base.py testcases/ --alluredir outputs/results/
 
+# run logs options
+#-vv -o log_cli=true -o log_cli_level=INFO --log-date-format="%Y-%m-%d %H:%M:%S" --log-format="%(filename)s:%(lineno)s %(asctime)s %(levelname)s %(message)s"
+
 echo "[PYTEST] generate allure report."
 allure generate outputs/results/ -o outputs/reports/ --clean
+# outputs: logs/, results/, reports/
 
 echo "[PYTEST] api test done."
 
