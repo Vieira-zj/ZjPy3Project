192d680cf1379706ac3cfe0072b8448104696ab0
zhengjin <zivieira@163.com>
2019-03-19 17:37:45 +0800

    Add py parallel demos.

--- pydemos/py_demo03_parallel.py
+++ pydemos/py_demo03_parallel.py
@@ -6,8 +6,8 @@ Created on 2019-03-14
 Python multiple process, threads and pool examples.
 '''
 
-import time
 import os
+import time
 
 
 # example 01, generator
@@ -208,27 +208,114 @@ def py_parallel_demo13():
     pass
 
 
-# example 14, 多线程池
-def py_parallel_demo14():
+# example 15, 多进程 生产消费模型
+def py_parallel_demo15():
     pass
 
 
-# example 21, 多进程 生产消费模型
+# example 16, 多进程 生产消费模型 阻塞队列
+def py_parallel_demo16():
+    pass
+
+
+# example 17, 多线程/多进程池 multiprocessing
+def multiple(x):
+    import threading
+    res = x * 2
+    print('[%s:%s] multiple results: %d' %(os.getpid(), threading.current_thread().getName(), res))
+
+def py_parallel_demo17(is_thread=False):
+    from multiprocessing import Pool as ProcessPool
+    from multiprocessing.dummy import Pool as ThreadPool
+
+    num_list = []
+    for num in range(100):
+        num_list.append(num)
+
+    pool = None
+    if is_thread:
+        pool = ThreadPool(3)
+    else:
+        pool = ProcessPool(3)
+
+    try:
+        pool.map(multiple, num_list)
+    finally:
+        if pool:
+            pool.close()
+            pool.join()
+
+
+# example 21, 多线程/多进程池 concurrent.futures
+def get_executor(is_thread=False):
+    from concurrent.futures import ThreadPoolExecutor
+    from concurrent.futures import ProcessPoolExecutor
+
+    return ThreadPoolExecutor() if is_thread else ProcessPoolExecutor(max_workers=3)
+
+def load_url(url):
+    import threading
+    import urllib.request as request
+
+    print('[%s:%s] running ...' % (os.getpid(), threading.current_thread().getName()))
+    with request.urlopen(url, timeout=5) as conn:
+        print('%r page is %d bytes' % (url, len(conn.read())))
+
 def py_parallel_demo21():
-    pass
+    from concurrent.futures import wait, as_completed
+
+    executor = get_executor()
+    urls = ['http://www.163.com', 'https://www.baidu.com/', 'https://github.com/']
+    f_list = []
+    for url in urls:
+        future = executor.submit(load_url, url)
+        f_list.append(future)
+    print(wait(f_list, return_when='ALL_COMPLETED'))
 
 
-# example 22, 多进程 生产消费模型 阻塞队列
+# example 22, 多线程/多进程池 concurrent.futures map
 def py_parallel_demo22():
-    pass
+    from concurrent.futures import wait, as_completed
+
+    urls = ['http://www.163.com', 'https://www.baidu.com/', 'https://github.com/']
+    executor = get_executor()
+    executor.map(load_url, urls)
 
 
-# example 23, 多进程池
+# example 23, 多进程池 concurrent.futures callback
+def get_page(url):
+    import requests
+    import threading
+
+    print('<%s:%s> get page [%s]' % (os.getpid(), threading.current_thread().getName(), url))
+    response = requests.get(url)
+    if response.status_code != 200:
+        raise ConnectionError('error get %s, and return code %d' %(url, response.status_code))
+    return {'url': url, 'text': response.text}
+
 def py_parallel_demo23():
-    pass
+    import threading
+
+    def parse_page(res):
+        res = res.result()
+        print('<%s:%s> parse page [%s]' % (os.getpid(), threading.current_thread().getName(), res['url']))
+        parse_line = 'url [%s], size: %s\n' % (res['url'], len(res['text']))
+        print(parse_line)
+        with open(os.path.join(os.getenv('HOME'), 'Downloads/tmp_files/test.out'), 'a') as f:
+            f.write(parse_line)
+
+    # main
+    executor = get_executor()
+    urls = ['http://www.163.com', 'https://www.baidu.com/', 'https://github.com/']
+    for url in urls:
+        # func "get_page()" must be global
+        executor.submit(get_page, url).add_done_callback(parse_page)
+
+    executor.shutdown()
+    print('main', os.getpid())
 
 
 if __name__ == '__main__':
 
-    py_parallel_demo12()
+    py_parallel_demo17()
     print('python parallel demo DONE.')
