9c8668f061973a02ba91e8c03b7bae3626a01839
zhengjin <zivieira@163.com>
2019-04-25 18:56:17 +0800

    Update pyspark demo.

--- pyspark/pyspark_demo_base.py
+++ pyspark/pyspark_demo_base.py
@@ -10,7 +10,7 @@ bin/spark-submit \
 --driver-memory 1g \
 --executor-memory 1g \
 --executor-cores 1 \
-/mnt/ds_test/pyspark_base.py
+/mnt/spark_dir/pyspark_demo_base.py
 '''
 
 from pyspark import SparkConf, SparkContext
@@ -18,21 +18,38 @@ from pyspark import SparkConf, SparkContext
 
 # init a rdd
 def pyspark_demo01(sc):
-    ls_rdd = sc.parallelize([i for i in range(60)])
-    print('rdd partitions size: ' + ls_rdd.getNumPartitions())
-    print('output:' + ls_rdd.map(lambda x: x+2).collect())
+    ls_rdd = sc.parallelize([i for i in range(60)]).cache()
+    print('rdd partitions size: %d' % ls_rdd.getNumPartitions())
+
+    map_output = ls_rdd.map(lambda x: x+2).collect()
+    print('map output: ' + str(map_output))
+    filter_output = ls_rdd.filter(lambda x: x % 2 == 0).collect()
+    print('filter output: ' + str(filter_output))
+
+
+# distinct
+def pyspark_demo02(sc):
+    def _doubleIfOdd(x):
+        return x if x % 2 == 0 else x * 2
+
+    num_rdd = sc.parallelize(range(1, 11))
+    result_rdd = num_rdd.map(_doubleIfOdd) \
+        .filter(lambda x: x > 6) \
+        .distinct()
+    print('result rdd: ' + str(result_rdd.collect()))
 
 
 # read hdfs text file
-def pyspark_demo02(sc):
-    # pre-condition: put file to hdfs
+def pyspark_demo03(sc):
+    # pre-condition: mkdir and put file on hdfs
     hdfs_path = '/user/root/wordcount/helloworld.txt'
-    rdd = sc.textFile(hdfs_path)
+    rdd = sc.textFile(hdfs_path).cache()
+    print('lines count: %d' % rdd.count())
     print('first line output: ' + rdd.first())
 
 
 # wordcount example
-def pyspark_demo03(sc):
+def pyspark_demo04(sc):
     from operator import add
 
     hdfs_path = '/user/root/wordcount/helloworld.txt'
@@ -50,6 +67,5 @@ if __name__ == "__main__":
     sc = SparkContext(conf=conf)
     print('pyspark version: ' + str(sc.version))
 
-    pyspark_demo03(sc)
-
+    pyspark_demo04(sc)
     print('pyspark base demo DONE.')
