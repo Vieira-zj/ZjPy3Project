359cd56e04d606e7a7a0200ca31662160a98ffb5
zhengjin <zivieira@163.com>
2019-04-29 12:13:32 +0800

    Add spark df demos.

--- pyspark/pyspark_df_base.py
+++ pyspark/pyspark_df_base.py
@@ -17,25 +17,8 @@ from pyspark import SparkConf, SparkContext
 from pyspark.sql import SQLContext
 
 
-# init a dataframe
-def pyspark_df_demo01(sqlContext):
-    records = []
-    records.append((1, 'age', '30', 50, 40))
-    records.append((1, 'city', 'beijing', 50, 40))
-    records.append((1, 'gender', 'fale', 50, 40))
-    records.append((1, 'height', '172cm', 50, 40))
-    records.append((1, 'weight', '70kg', 50, 40))
-
-    schema = ['user_id', 'attr_name', 'attr_value', 'income', 'expenses']
-    df = sqlContext.createDataFrame(records, schema)
-    print('data schema: ' + str(df.dtypes))
-    print('user count: ' + str(df.count()))
-    print('users info:')
-    df.show()
-
-
 # read hdfs json file
-def pyspark_df_demo02(sqlContext):
+def pyspark_df_demo01(sqlContext):
     df = sqlContext.read.format('json').load('/user/root/test/people.json')
     # {"name":"Michael"}
     # {"name":"Andy", "age":30}
@@ -46,7 +29,43 @@ def pyspark_df_demo02(sqlContext):
     df.show()
 
 
-# select in dataframe
+# init a dataframe and query
+def pyspark_df_demo02(sqlContext):
+    records = []
+    records.append((1, 'age', '30', 50, 40))
+    records.append((1, 'city', 'beijing', 50, 40))
+    records.append((1, 'gender', 'fale', 50, 40))
+    records.append((1, 'height', '172cm', 50, 40))
+    records.append((1, 'weight', '70kg', 50, 40))
+
+    records.append((2, 'age', '26', 100, 80))
+    records.append((2, 'city', 'beijing', 100, 80))
+    records.append((2, 'gender', 'fale', 100, 80))
+    records.append((2, 'height', '170cm', 100, 80))
+    records.append((2, 'weight', '65kg', 100, 80))
+
+    schema = ['user_id', 'attr_name', 'attr_value', 'income', 'expenses']
+    df = sqlContext.createDataFrame(records, schema)
+    print('data schema:')
+    df.printSchema()
+    print('user count: ' + str(df.count()))
+    print('users info:')
+    df.show(5)
+
+    # print('[distinct output] user ids:')
+    # df.select('user_id').distinct().show()
+
+    print('[select output] user info with income=50:')
+    df.select('user_id', 'attr_value', 'income').where('income=50').show()
+
+    print('[orderby output] user info with income desc seq:')
+    df.orderBy(df.income.desc()).show()
+
+    print('[col update output] user info with new income:')
+    df.withColumn('income_new', df.income+10).show(5)
+
+
+# dataframe sql
 def pyspark_df_demo03(sqlContext):
     from pyspark.sql.types import StructType, StructField, StringType, LongType
 
@@ -66,21 +85,44 @@ def pyspark_df_demo03(sqlContext):
     print('data schema: ' + str(df.dtypes))
     print('swimmers count: ' + str(df.count()))
 
-    print('[select output]: get swimmers ids with age=22 by str:')
+    print('swimmers:')
+    df.show()
+
+    print('[select output] get swimmers ids with age=22 by str:')
     df.select('id', 'age').filter('age=22').show()
-    print('[select output]: get swimmers ids with age=22 by type:')
+    print('[select output] get swimmers ids with age=22 by type:')
     df.select(df.id, df.age).filter(df.age == 22).show()
 
-    print('[select output]: get swimmers ids with age=22 by str sql:')
+    print('[select output] get swimmers ids with age=22 by str sql:')
     df.registerTempTable('swimmers')
     sqlContext.sql('select id,age from swimmers where age=22').show()
+
+    print('[like output] get swimmers names with eye color startwith "b":')
+    df.select('name', 'eyeColor').filter('eyeColor like "b%"').show()
+    print('[like output] get swimmers names with eye color startwith "b" by str sql:')
+    str_sql = 'select name,eyeColor from swimmers where eyeColor like "b%"'
+    sqlContext.sql(str_sql).show()
+
+
+# dataframe join
+def pyspark_df_demo04(sqlContext):
+    # same data schema
+    sentence_df_01 = sqlContext.createDataFrame((
+        (1, 'asf'),
+        (2, '2143'),
+        (3, 'rfds')
+    )).toDF('label', 'sentence')
+    sentence_df_01.show()
+
+    # TODO:
 
 
 if __name__ == '__main__':
 
     conf = SparkConf().setAppName('pyspark_df_base_test').setMaster('yarn-client')
     sc = SparkContext(conf=conf)
+    print('pyspark version: ' + str(sc.version))
     sqlContext = SQLContext(sc)
 
-    pyspark_df_demo01(sqlContext)
+    pyspark_df_demo02(sqlContext)
     print('pyspark dataframe base demo DONE.')
