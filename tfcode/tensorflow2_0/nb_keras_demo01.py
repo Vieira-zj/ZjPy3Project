59478611a82ccde72a8096b7b94b347482be7a94
zhengjin <zivieira@163.com>
2020-03-25 21:54:13 +0800

    Add tf keras demo.

--- tfcode/tensorflow2_0/nb_keras_demo01.py
+++ tfcode/tensorflow2_0/nb_keras_demo01.py
@@ -1,71 +1,28 @@
 # %%
+# cnn for y = 2 * x - 1
 import tensorflow as tf
-from tensorflow.keras import layers
-tf.__version__, tf.keras.__version__
+import numpy as np
+from tensorflow import keras
+tf.__version__, keras.__version__
 
 # %%
-# 构建模型
-model = tf.keras.Sequential()
-model.add(layers.Dense(32, activation='relu'))
-model.add(layers.Dense(32, activation='relu'))
-model.add(layers.Dense(10, activation='softmax'))
-print('define layers of model')
-
-# %%
-model.compile(optimizer=tf.keras.optimizers.Adam(0.001),
-              loss=tf.keras.losses.categorical_crossentropy,
-              metrics=[tf.keras.metrics.categorical_accuracy])
+# 创建一个具有1层的神经网络，该层具有1个神经元
+model = tf.keras.Sequential([keras.layers.Dense(units=1, input_shape=[1])])
+# 损失函数 mean_squared_error
+# 损失函数将猜测的答案与已知的正确答案进行比较，并衡量其结果的好坏
+# 机器学习模型还需要优化器函数。根据损失函数的运行情况，它将尝试使损失最小化
+model.compile(optimizer='sgd', loss='mean_squared_error')
 model.summary()
 
 # %%
-# 模型训练
-import numpy as np
-np.random.seed(66)
-
-train_x = np.random.random((1000, 72))
-train_y = np.random.random((1000, 10))
-val_x = np.random.random((200, 72))
-val_y = np.random.random((200, 10))
-
-model.fit(train_x, train_y, epochs=10, batch_size=100,
-          validation_data=(val_x, val_y))
-print('model train by np array')
+# 模型将尝试500次迭代优化它的权重
+xs = np.array([-1.0,  0.0, 1.0, 2.0, 3.0, 4.0], dtype=float)
+ys = np.array([-3.0, -1.0, 1.0, 3.0, 5.0, 7.0], dtype=float)
+model.fit(xs, ys, epochs=100)
 
 # %%
-model.fit(train_x, train_y, epochs=10, batch_size=100,
-          validation_split=0.2)
-print('model train and validation_split')
-
-# %%
-# 使用tf.data构建训练输入
-dataset = tf.data.Dataset.from_tensor_slices((train_x, train_y))
-dataset = dataset.batch(32)
-dataset = dataset.repeat()
-val_dataset = tf.data.Dataset.from_tensor_slices((val_x, val_y))
-val_dataset = val_dataset.batch(32)
-val_dataset = val_dataset.repeat()
-
-model.fit(dataset, epochs=10, steps_per_epoch=30,
-          validation_data=val_dataset, validation_steps=3)
-print('model train by tf dataset')
-
-# %%
-# 评估与预测
-test_x = np.random.random((1000, 72))
-test_y = np.random.random((1000, 10))
-model.evaluate(test_x, test_y, batch_size=32)
-print('modle evaluate by np array')
-
-# %%
-test_data = tf.data.Dataset.from_tensor_slices((test_x, test_y))
-test_data = test_data.batch(32).repeat()
-model.evaluate(test_data, steps=30)
-print('modle evaluate by tf dataset')
-
-# %%
-result = model.predict(test_x, batch_size=32)
-print(result.shape)
-result[0]
+pred = model.predict([10.0])
+pred
 
 # %%
 print('tf keras demo done')
