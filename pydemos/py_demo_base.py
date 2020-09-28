a7d1dfeb2d2378677e48126da52aab18be08ab27
zhengjin <zivieira@163.com>
2020-09-28 15:14:14 +0800

    Update pydantic model demo.

--- pydemos/py_demo_base.py
+++ pydemos/py_demo_base.py
@@ -5,8 +5,6 @@ Created on 2018-10-31
 '''
 
 from collections import deque, defaultdict, Counter
-from pydantic import BaseModel, ValidationError, validator
-from typing import List
 
 from datetime import datetime
 import getopt
@@ -925,63 +923,6 @@ def py_base_ex36():
     test_list = ['a', 'b', 'd']
     test_list.insert(2, 'c')
     print(test_list)
-
-
-# example 37-1, pydantic validate for model
-class User(BaseModel):
-    id: int
-    name: str = "chr"
-    ts: datetime = None
-    numbers: List[int] = []
-
-
-def py_base_ex3701():
-    data = {
-        "id": 123,
-        "name": "test",
-        "ts": datetime.now(),
-        "numbers": [1, 2, "a"]
-    }
-    try:
-        user = User(**data)
-        print(user)
-    except ValidationError as e:
-        print(e.json())
-
-
-# example 37-2, pydantic validate for model
-class UserModel(BaseModel):
-    name: str
-    username: str
-    password1: str
-    password2: str
-
-    @validator('name')
-    def name_must_contain_space(cls, v):
-        if ' ' not in v:
-            raise ValueError('must contain a space')
-        return v
-
-    @validator('username')
-    def username_alphanumeric(cls, v):
-        assert v.isalpha(), 'must be alphanumeric'
-        return v
-
-    @validator('password2')
-    def passwords_match(cls, v, values, **kwargs):
-        if 'password1' in values and v != values['password1']:
-            raise ValueError('passwords do not match')
-        return v
-
-
-def py_base_ex3702():
-    try:
-        print(UserModel(name='samuel colvin', username='scolvin',
-                        password1='zxcvbn', password2='zxcvbn'))
-        print(UserModel(name='samuel', username='scolvin',
-                        password1='zxcvbn', password2='zxcvbn2'))
-    except ValidationError as e:
-        print("error:", e)
 
 
 # example 38, read chrome cookie (sqlite.db)
