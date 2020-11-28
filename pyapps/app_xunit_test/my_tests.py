ae55bc096b0ac38322e2f0b68a66bcd12f3ab26c
zhengjin <zivieira@163.com>
2020-11-28 17:47:42 +0800

    Add inspect demo.

--- pyapps/app_xunit_test/my_tests.py
+++ pyapps/app_xunit_test/my_tests.py
@@ -17,13 +17,10 @@ class MyMetaClass01(type):
     """ metaclass __new__ """
 
     def __new__(typ, classname, bases, class_dict):
-        tests: List[Callable] = []
-        for item in class_dict.values():
-            if callable(item) and hasattr(item, 'test_meta'):
-                tests.append(item)
-
+        tests: List[Callable] = [item for item in class_dict.values(
+        ) if callable(item) and hasattr(item, 'test_meta')]
         if len(tests) > 0:
-            print(classname, 'inject tests')
+            print(classname, 'inject tests:', [t.__name__ for t in tests])
             class_dict['tests'] = tests
 
         return type.__new__(typ, classname, bases, class_dict)
@@ -52,13 +49,10 @@ class MyMetaClass02(type):
     """ metaclass __init__ """
 
     def __init__(mcs, classname, bases, class_dict):
-        tests: List[Callable] = []
-        for item in class_dict.values():
-            if callable(item) and hasattr(item, 'test_meta'):
-                tests.append(item)
-
+        tests: List[Callable] = [item for item in vars(
+            mcs).values() if callable(item) and hasattr(item, 'test_meta')]
         if len(tests) > 0:
-            print(classname, 'inject tests')
+            print(classname, 'inject tests:', [t.__name__ for t in tests])
             assert not hasattr(mcs, 'tests')
             setattr(mcs, 'tests', tests)
 
@@ -73,15 +67,15 @@ class MyTestSuite02(MyTestBase02):
 
     @test_desc('test case 11, foo')
     @test_meta(TestMeta(title='case11', priority=1))
-    def testCase01(self):
+    def testCase11(self):
         print('This is test case 11, say foo')
 
     @test_desc('test case 12, bar')
     @test_meta(TestMeta(title='case12', priority=2))
-    def testCase02(self):
+    def testCase12(self):
         print('This is test case 12, say bar')
 
-    def testCase03(self):
+    def testCase13(self):
         print('This is test case 13, hello')
 
 
@@ -93,4 +87,15 @@ class MyTestSuite03(MyTestBase02):
 
 if __name__ == '__main__':
 
-    pass
+    import inspect
+    print('get test funcs in class by inspect:')
+    for clazz in (MyTestSuite01, MyTestSuite02):
+        tests = {name: func for name, func in inspect.getmembers(
+            clazz, inspect.isfunction) if hasattr(func, 'test_meta')}
+        print(f"[{clazz.__name__}] includes test cases: {list(tests.keys())}")
+
+    print('\nget test funcs in class by vars():')
+    for clazz in (MyTestSuite01, MyTestSuite02):
+        tests = {name: func for name, func in vars(
+            clazz).items() if callable(func) and hasattr(func, 'test_meta')}
+        print(f"[{clazz.__name__}] includes test cases: {list(tests.keys())}")
